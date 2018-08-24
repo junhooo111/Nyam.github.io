70f697107046279340f62e35bcf781cbd936c6c5
jelliyjane <rean5123@naver.com>
2018-08-24 21:02:02 +0900

    location

--- searchtest/app/models/book.rb
+++ searchtest/app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
     
     def self.search(search)
         if search
-            where(["name LIKE ?","%#{search}%"])
+            where(["location LIKE ?","%#{search}%"])
         else
             all
         end
